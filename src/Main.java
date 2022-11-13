c67585ad777a3b5f2cfb31b7687750ede053eb41
zhuang002 <zhuang001@gmail.com>
2022-11-13 11:47:01 -0500

    using hashmap

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-	
+	static HashMap<Character, Integer> pressMap = new HashMap<>();
+	static HashMap<Character, Integer> keyMap = new HashMap<>();
 	static String[] map = {"_", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		for (int i=2;i<map.length;i++) {
+			String s = map[i];
+			for (int j=0;j<s.length();j++) {
+				char c = s.charAt(j);
+				pressMap.put(c,j+1);
+				keyMap.put(c, i);
+			}
+		}
 		
 		
 		Scanner sc = new Scanner(System.in);
@@ -35,9 +44,13 @@ public class Main {
 
 	private static int getCharTime(char preChar, char c) {
 		// TODO Auto-generated method stub
-		int keyPre = getKey(preChar);
-		int key = getKey(c);
-		int press = getPress(c, key);
+		int keyPre;
+		if (preChar == '\0') 
+			keyPre = -1;
+		else
+			keyPre = keyMap.get(preChar);
+		int key = keyMap.get(c);
+		int press = pressMap.get(c);
 		int time = press;
 		if (keyPre == key) {
 			time += 2;
@@ -45,23 +58,7 @@ public class Main {
 		return time;
 	}
 
-	private static int getPress(char c, int key) {
-		// TODO Auto-generated method stub
-		String s = map[key];
-		return s.indexOf(c)+1;
-	}
-
-	private static int getKey(char c) {
-		// TODO Auto-generated method stub
-		for (int i=0;i<map.length;i++) {
-			String s = map[i];
-			if (s.indexOf(c) >=0) {
-				return i;
-			}
-		}
-		return -1;
-	}
-	
+		
 	
 
 	
